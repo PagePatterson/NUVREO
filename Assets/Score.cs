d2124cfb1771d20a49fcf7ceda5d9104df3b6681
aishabigham <aishabigham21@gmail.com>
2024-11-18 18:40:27 -0500

    Update Score script

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -23,12 +23,26 @@ public class Score : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateScoreUI();
+        ClearResultMessage();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         if (scoreText != null) {
+            scoreText.text = "Score: " + score.ToString();
+        }
+    }
+
+    public void gameOverChecker() {
+        if (abs(player1Score - player2Score) >= 2 and (player1Score >= 21 or player2Score >= 21)) {
+            if (player1Score > player2Score) {
+                DisplayResultMessage("Player 1 Wins!")
+            }
+            else {
+                DisplayResulMessage("Player 2 Wins!");
+            }
+        }
     }
 }
